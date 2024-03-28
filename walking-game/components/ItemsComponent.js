9646f8955a76132008093ef71dc35f05920c9d85
Petri Kilpelainen <hunterpete2002@gmail.com>
2024-03-28 05:19:00 +0200

    Basic item upgrade system.

--- walking-game/components/ItemsComponent.js
+++ walking-game/components/ItemsComponent.js
@@ -5,18 +5,75 @@ import { state$ } from "./states";
 export default function ItemsComponent() {
 
     allItemsDict = {
-        baseBoots: {id: 1, name: "Really bad boots.", effect:{walkingMultiplier: 5}, cost: 1},
-        sunGlasses: {id: 2, name: "Bad sunglasses.", effect:{walkingPower: 5}, cost: 1},
-        goodSunGlasses: {id: 3, name: "Good sunglasses.", effect:{walkingPower: 1, walkingMultiplier: 1}, cost: 1}
+        baseBoots: { name: "Really bad boots.", effect:{walkingMultiplier: 5}, cost: 800},
+        sunGlasses: { name: "Bad sunglasses.", effect:{walkingPower: 5}, cost: 223},
+        goodSunGlasses: { name: "Good sunglasses.", effect:{walkingPower: 1, walkingMultiplier: 1}, cost: 1232}
 
     }
+
+    const [upgradeItemsDict, setUpgradeItemsDict] = useState({
+        baseBoots: { effect:{walkingMultiplier: 0.5}, cost: 10, costMult: 1.2, level: 1},
+        sunGlasses: { effect:{walkingPower: 0.5}, cost: 10, costMult: 1.2, level: 1},
+        goodSunGlasses: { effect:{walkingPower: 0.1, walkingMultiplier: 0.1}, cost: 100, costMult: 1.1, level: 1}
+    })
+
     const [ownedItems, setOwnedItems] = useState([]);
+
+    const [resource, setResource] = useState(10000000); // Temporary for testing
+
     useEffect(() => {
         ownedItems.forEach(itemId => {
             console.log(allItemsDict[itemId].name);
         }); 
 
     }, [ownedItems]);
+
+    const buyItem = (itemId) => {
+        const itemCost = allItemsDict[itemId].cost
+
+        if (resource > itemCost) {
+            console.log("Buying" + itemId);
+            setResource(resource - itemCost);
+            addItemToInventory(itemId);
+            console.log(resource)
+
+        } else {
+            console.log("Too poor.")
+        }
+    }
+
+    const upgradeItem = (itemId) => {
+        const upgradeCost = upgradeItemsDict[itemId].cost
+
+        console.log("Upgrade cost: " + upgradeCost);
+
+        if (resource > upgradeCost) {
+            console.log("Upgrading: " + itemId);
+            setResource(resource - upgradeCost);
+            console.log(resource);
+
+            const updatedUpgradeItemsDict = { ...upgradeItemsDict };
+            updatedUpgradeItemsDict[itemId].cost *= updatedUpgradeItemsDict[itemId].costMult;
+            console.log("New cost: " + updatedUpgradeItemsDict[itemId].cost);
+
+            updatedUpgradeItemsDict[itemId].level++;
+            setUpgradeItemsDict(updatedUpgradeItemsDict);
+
+            const itemEffects = upgradeItemsDict[itemId].effect;
+            for (const effectKey in itemEffects) {
+                if (itemEffects.hasOwnProperty(effectKey)) {
+                    let prevStateMod = state$.modifiers[effectKey].get();
+                    let newStateMod = prevStateMod + itemEffects[effectKey];
+                    state$.modifiers[effectKey].set(newStateMod);
+                }
+            }
+            console.log("Walking power: " + state$.modifiers.walkingPower.get())
+            console.log("Walking mult: " + state$.modifiers.walkingMultiplier.get())    
+        } else {
+            console.log("Too poor.")
+        }
+
+    }
 
     const addItemToInventory = (itemId) => {
         if (ownedItems.includes(itemId)){
@@ -33,7 +90,7 @@ export default function ItemsComponent() {
                     let prevStateMod = state$.modifiers[effectKey].get();
                     let newStateMod = prevStateMod + itemEffects[effectKey];
                     state$.modifiers[effectKey].set(newStateMod);
-                            }
+                }
             }
 
             console.log(state$.modifiers.walkingPower.get())
@@ -57,7 +114,7 @@ export default function ItemsComponent() {
                                 <Text>{item.name}</Text>
                                 <Button 
                                     onPress={() => {
-                                        addItemToInventory(itemKey);
+                                        buyItem(itemKey);
                                     }}
                                     title={`Buy ${item.name}.`}
                                 />
@@ -72,6 +129,7 @@ export default function ItemsComponent() {
                     <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Owned Items:</Text>
                         {ownedItems.map(itemKey => {
                             const item = allItemsDict[itemKey];
+                            const itemUpgrade = upgradeItemsDict[itemKey];
                             return (
                                 <View key={item.id}>
                                     <Text>Name: {item.name}</Text>
@@ -79,12 +137,18 @@ export default function ItemsComponent() {
                                     {Object.entries(item.effect).map(([effectKey, value]) => (
                                         <Text key={effectKey}>{effectKey}: {value}</Text>
                                     ))}
+                                    <Text> Level: {itemUpgrade.level}</Text>
+                                    <Button 
+                                        onPress={() => {
+                                            upgradeItem(itemKey);
+                                        }}
+                                        title={`Upgrade ${item.name}.`}
+                                    />
                                 </View>
                             );
                         })}
                 </View>
             )}
-
         </View>
     )
 }
--- walking-game/components/gameScreen.js
+++ walking-game/components/gameScreen.js
@@ -1,4 +1,4 @@
-import { observe,computed } from "@legendapp/state";
+import { observe, computed } from "@legendapp/state";
 import { state$, walkingResult } from "./states";
 import { useInterval } from "usehooks-ts"
 import { Text } from "react-native";
@@ -6,16 +6,16 @@ import { Text } from "react-native";
 export default function GameScreen() {
 
 
-useInterval(() => {
-  const newStrengthXp = (state$.skills.strength.xp.get()) + walkingResult.get();
-  state$.skills.strength.xp.set(newStrengthXp);
-  
-}, 1000);
-const strengthXp = state$.skills.strength.xp.get()
+  useInterval(() => {
+    const newStrengthXp = (state$.skills.strength.xp.get()) + walkingResult.get();
+    state$.skills.strength.xp.set(newStrengthXp);
+    //console.log(state$.skills.strength.xp.get())
+  }, 1000);
+  const strengthXp = state$.skills.strength.xp.get()
 
-return (
-  <Text>Strength XP: {strengthXp}</Text>
-);
-    
+  return (
+    <Text>Strength XP: {strengthXp}</Text>
+  );
+
 
 }
